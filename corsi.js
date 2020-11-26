e36bc9e74cff2c234d48132469960d14d756ee59
accademiadiquartiere <accademiadiquartiere@gmail.com>
2020-11-26 16:53:16 +0100

    Aggiornamento corsi

--- corsi.js
+++ corsi.js
@@ -3,30 +3,25 @@ var corsi = [{
     descrizione: " l'associazione fa parte della rete Diritto di Parola del Comune di Reggio Emilia e offre corsi di italiano che partono dal livello base ALFA - preA1 al livello B2",
     quando: "<br/>&nbsp;<i>PRE-A1:</i> " +
         "       <br/>&nbsp;&nbsp;&nbsp;Lunedì - Mercoledì 9:30 - 11:30" +
-        "       <br/>&nbsp;&nbsp;&nbsp;Martedì - Giovedì 9:30 - 11:30" +
-        "       <br/>&nbsp;&nbsp;&nbsp;Sabato e Domenica Chiusi<br/><br/>" +
+        "       <br/>&nbsp;&nbsp;&nbsp;Martedì - Giovedì 9:30 - 11:30<br/><br/>" +
         "       <i>&nbsp;A2:</i>" +
-        "       <br/>&nbsp;&nbsp;&nbsp;Martedì - Giovedì 10 - 12" +
-        "       <br/>&nbsp;&nbsp;&nbsp;Sabato e Domenica Chiusi<br/><br/>" +
+        "       <br/>&nbsp;&nbsp;&nbsp;Martedì - Giovedì 10 - 12<br/><br/>" +
         "       <i>&nbsp;B1/B2</i>: " +
-        "       <br/>&nbsp;&nbsp;&nbsp;Martedì - Giovedì 15 - 17" +
-        "       <br/>&nbsp;&nbsp;&nbsp;Sabato e Domenica Chiusi<br/>",
+        "       <br/>&nbsp;&nbsp;&nbsp;Martedì - Giovedì 15 - 17<br/>",
     prezzo: " per chi frequenta i corsi di italiano è di 25 euro per i corsi pre-A1 e A2, 30 euro per il corso B1/B2.<br/>Per gli adulti che frequentano altre attività dell’associazione il costo annuale è di 60 euro.",
     immagine: "linguaItaliana.jpg"
 }, {
     titolo: "Doposcuola",
     descrizione: "il servizio di supporto allo studio segue il protocollo di Officina Educativa, in modo che i bambini e i ragazzi possano studiare e fare i compiti con l'aiuto dei nostri educatori in un ambiente sicuro e controllato.",
-    quando: "<br/>&nbsp;&nbsp;Lunedì, Martedì, Mercoledì, Giovedì: 15 – 17 / 17 – 19" +
-        "<br/>&nbsp;&nbsp;Sabato e Domenica Chiusi<br/>",
-    prezzo: " Gratis",
+    quando: "<br/>&nbsp;&nbsp;Lunedì, Martedì, Mercoledì, Giovedì: 15 – 17 / 17 – 19<br/>",
+    prezzo: " Incluso nel costo di iscrizione annuale di 30€",
     immagine: "doposcuola.jpg"
 
 }, {
     titolo: "Corso di arti figurative",
     descrizione: "il maestro e fotografo Maurizio Mantovi, grazie alla sua esperienza decennale sul campo, introduce la storia dell'arte e insegna ai ragazzi le principali tecniche di disegno",
-    quando: "<br/>&nbsp;&nbsp;&nbsp;Ogni Venerdì dalle 17 alle 18:30" +
-        "<br/>&nbsp;&nbsp;&nbsp;Sabato e Domenica Chiusi<br/>",
-    prezzo: " Gratis",
+    quando: "<br/>&nbsp;&nbsp;&nbsp;Ogni Venerdì dalle 17 alle 18:30<br/>",
+    prezzo: " Incluso nel costo di iscrizione annuale di 30€",
     immagine: "artiFigurative.jpg"
 
 }, {
@@ -35,9 +30,8 @@ var corsi = [{
     quando: "<br/>&nbsp;&nbsp;<i>Primo gruppo:</i> " +
         "<br/>&nbsp;&nbsp;Lunedì - Mercoledì 17:00 - 18:30<br/><br/>&nbsp;&nbsp;" +
         "<i>Secondo Gruppo</i>:" +
-        "<br/>&nbsp;&nbsp;Martedì - Giovedì 19 - 20:30<br/><br/>&nbsp;&nbsp;" +
-        "Sabato e Domenica Chiusi<br/>",
-    prezzo: " Gratis",
+        "<br/>&nbsp;&nbsp;Martedì - Giovedì 19 - 20:30<br/>&nbsp;&nbsp;",
+    prezzo: " Incluso nel costo di iscrizione annuale di 30€",
     immagine: "arabo.jpg"
 
 }]
