010c0795eb749cf0a4258540b7e97d065f609f42
accademiadiquartiere <accademiadiquartiere@gmail.com>
2020-12-22 11:08:20 +0100

    Add Volti

--- sezioniModificabili/volti.js
+++ sezioniModificabili/volti.js
@@ -1,10 +1,47 @@
-var volti = [{
-    nome: "Maurizio Ientile",
-    foto: "maurizioientile.jpg",
-    ruolo: "Presidente"
-}, {
-    nome: "Silvia Morelli",
-    foto: "silviamorelli.jpg",
-    ruolo: "Coordinatrice"
-}
+var volti = [
+    {
+        nome: "Maurizio",
+        foto: "maurizio1.jpg",
+        ruolo: "Presidente"
+    },
+    {
+        nome: "Silvia",
+        foto: "silvia.jpg",
+        ruolo: "Coordinatrice"
+    },
+    {
+        nome: "Licia",
+        foto: "licia1.jpg",
+        ruolo: "Volontaria"
+    },
+    {
+        nome: "Omaima",
+        foto: "omaima.jpg",
+        ruolo: "Volontarioa"
+    },
+    {
+        nome: "Mirco",
+        foto: "mirco1.jpg",
+        ruolo: "Maestro di Musica"
+    },
+    {
+        nome: "Carla",
+        foto: "carla.jpg",
+        ruolo: "Volontaria"
+    },
+    {
+        nome: "Flavia",
+        foto: "flavia.jpg",
+        ruolo: "Volontaria"
+    },
+    {
+        nome: "Roger",
+        foto: "roger.jpg",
+        ruolo: "Capo della Logistica"
+    },
+    {
+        nome: "Nedra",
+        foto: "nedra.jpg",
+        ruolo: "Insegnante"
+    }
 ];
