34d4fdace07c73e63fe75177d439977893914bc1
accademiadiquartiere <73605955+accademiadiquartiere@users.noreply.github.com>
2021-06-16 19:39:49 +0200

    Update news.js

--- sezioniModificabili/news.js
+++ sezioniModificabili/news.js
@@ -1,18 +1,22 @@
-var news = [{ titoloNews: "Aperte le iscrizioni per il campo giochi “Estate Villa Sesso”",
-    sottotitolo: "Il campo giochi estivo di Accademia di quartiere, in collaborazione con Officina Educativa,\n" +
-        "è rivolto è rivolto ai ragazzi dai 6 ai 12 anni",
-    testoNews: "<p>Sono aperte le iscrizioni per il campo giochi <b>\"Estate Villa Sesso\"</b> in programma <b>dal 14 giugno al 30 luglio 2021</b>.</p>\n" +
+var news = [{ titoloNews: "I campi gioco “Estate Paradisi” ed “Estate Villa Sesso” proseguono fino al 30 luglio",
+    sottotitolo: "Il campo giochi estivo di Accademia di quartiere, in collaborazione con Officina Educativa, è rivolto è rivolto ai ragazzi dai 6 ai 12 anni",
+    testoNews: "<p>A Reggio Emilia e Villa Sesso proseguono fino al 30 luglio i campi gioco estivi di Accademia di Quartiere in collaborazione con Officina Educativa del Comune di Reggio nell'Emilia. </p>\n" +
         "\n" +
-        "<p>Il campo giochi estivo – nato da una proposta di <b>Accademia di quartiere</b> in collaborazione con <b>Officina Educativa</b> del Comune di Reggio nell'Emilia – è rivolto ai ragazzi <b>dai 6 ai 12 anni</b> e le attività si svolgeranno a Villa Sesso dal lunedì al venerdì, dalle 16 alle 19.30.</p>\n" +
-        "\n" +
-        "<p>Sarà un'estate in cui stare insieme, divertirsi e imparare. Il <b>ricco programma</b> di \"Estate Villa Sesso\" proporrà giochi, gite, momenti da dedicare ai compiti delle vacanze, il laboratorio di teatro a cura di Noveteatro e molto altro.</p>\n" +
-        "\n" +
-        "<p>L'iscrizione costa <b>25 euro</b> a settimana, la quota può essere <b>rimborsata</b> e i nostri operatori sono disponibili a spiegare come ottenere il rimborso.</p>\n" +
-        "\n" +
-        "<p>Per info e iscrizioni è possibile chiamare il numero <b>333 7876797</b> – attivo tutti i giorni dalle 15 alle 19 – o scrivere a <a href=mailto:accademiadiquartiere@gmail.com>accademiadiquartiere@gmail.com</a>.</p>\n",
-    foto: "estate-villa-sesso.jpg"
+"<p>“ESTATE PARADISI”, CORSI DI CANTO E ORTO IN TERRAZZA</p>\n" +
+        "<p>Il campo giochi estivo è rivolto ai ragazzi dai 6 ai 15 anni e le attività si svolgono in via Paradisi 10 a Reggio Emilia dal lunedì al venerdì, dalle 9 alle 13. In programma corsi di canto, orto in terrazza, visite ai Musei, momenti da dedicare ai compiti delle vacanze, corsi di inglese e spagnolo, letture e molto altro.</p>\n" + 
+"<p>Per info e iscrizioni: 353 4024417 (tutti i giorni dalle 10 alle 13) – <a href=mailto:accademiadiquartiere@gmail.com>accademiadiquartiere@gmail.com</a></p> \n" +
+"\n" +
+
+"<p>“ESTATE VILLA SESSO”, GIOCHI, GITE E LABORATORIO DI TEATRO</p> \n" +
+"<p>Il campo giochi estivo è rivolto ai ragazzi dai 6 ai 12 anni. Le attività si svolgono presso la Sala Polivalente in via Galeotti 6 a Villa Sesso (RE) dal lunedì al venerdì, dalle 16 alle 19.30. In programma giochi, gite, momenti da dedicare ai compiti delle vacanze, il laboratorio di teatro a cura di Noveteatro e molto altro.</p> \n" +
+"<p>Per info e iscrizioni a “Estate Villa Sesso”: 333 7876797 (tutti i giorni dalle 15 alle 19) – <a href=mailto:accademiadiquartiere@gmail.com>accademiadiquartiere@gmail.com</a></p>\n" +
+
+"<p>L'iscrizione costa <b>25 euro</b> a settimana, la quota può essere <b>rimborsata</b> e i nostri operatori sono disponibili a spiegare come ottenere il rimborso.</p>\n",
+    foto: "cover-facebook-campi.jpg"
 
 },{
+    
+    
     titoloNews: "Accademia di Quartiere fa la sua parte per i vaccini",
     sottotitolo: "Da lunedì 21 marzo il progetto recare permetterà alle persone non autosufficienti di prenotare il trasporto verso il punto vaccinale, attivo un numero di telefono",
     testoNews: "<p style='text-align:justify'\"><b>Accademia di Quartiere vuole fare la sua parte</b> in questo momento difficile, cercando di mettere a disposizione mezzi e risorse. <b>Nell’ambito del progetto RE-CARE ha organizzato un servizio di trasporto verso le sedi vaccinali per le persone non autosufficienti. Il servizio è totalmente gratuito</b> e prevede la copertura del percorso dall’abitazione fino alla sede della vaccinazione e ovviamente anche il ritorno a casa. <b>Per potersi prenotare è necessario chiamare il numero 353-4024417 nei giorni lunedì, martedì, giovedì, venerdì e sabato dalle 11.00 alle 13.00.</b> Un’operatrice dedicata di Accademia di Quartiere si occuperà di gestire le prenotazioni e gli appuntamenti su tutto il territorio di Reggio Emilia.</p> " +
